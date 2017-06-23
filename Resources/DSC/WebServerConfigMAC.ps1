b81c59136c6ce45f06daa81bf2222eaaa63897a3
lpalerm <lisa.palermo@mckesson.com>
2017-06-23 07:12:28 -0500

    DSC zip for MAC

---
+++ Resources/DSC/WebServerConfigMAC.ps1
@@ -0,0 +1,306 @@
+
+Configuration WebServerConfig
+{
+   param 
+   ( 
+       # [Parameter(Mandatory)]
+       # [String]$DomainName,
+
+       # [Parameter(Mandatory)]
+       # [System.Management.Automation.PSCredential]$Admincreds,
+
+       # [Int]$RetryCount=20,
+       # [Int]$RetryIntervalSec=30,
+
+        [Parameter(Mandatory)]
+        [String]$StorageAccountName,
+
+        [Parameter(Mandatory)]
+        [String]$StorageAccountContainer,
+
+        [Parameter(Mandatory)]
+        [String]$StorageAccountKey
+
+    )
+    Import-DscResource -ModuleName PSDesiredStateConfiguration, xPendingReboot, xAzureStorage #xSQLServer    
+
+	Node ("localhost")
+	{
+		#Install the IIS Role
+		WindowsFeature IIS
+		{
+			Ensure = "Present"
+			Name = "Web-Server"
+		}
+		
+		#Install Web Server
+		WindowsFeature WebWebServer
+		{
+			Ensure = "Present"
+			Name = "Web-WebServer"
+		}
+		#Install Web Directory Browsing
+		WindowsFeature WebDirBrowsing
+		{
+			Ensure = "Present"
+			Name = "Web-Dir-Browsing"
+		}
+
+		#Install Web HTTP Errors
+		WindowsFeature WebHttpErrors
+		{
+			Ensure = "Present"
+			Name = "Web-Http-Errors"
+		}
+
+		#Install Net HTTP Activation
+		WindowsFeature NETHTTPActivation
+		{
+			Ensure = "Present"
+			Name = "NET-HTTP-Activation"
+		}
+
+		#Install Web Mgmt Tools
+		WindowsFeature WebMgmtTools
+		{
+			Ensure = "Present"
+			Name = "Web-Mgmt-Tools"
+		}
+
+		#Install Web Mgmt Compatibility
+		WindowsFeature WebMgmtCompat
+		{
+			Ensure = "Present"
+			Name = "Web-Mgmt-Compat"
+		}
+
+		#Install Web Metabase
+		WindowsFeature WebMetabase
+		{
+			Ensure = "Present"
+			Name = "Web-Metabase"
+		}
+
+		#Install Web Performance
+		WindowsFeature WebPerformance
+		{
+			Ensure = "Present"
+			Name = "Web-Performance"
+		}
+
+		#Install Web HTTP Logging
+		WindowsFeature WebHttpLogging
+		{
+			Ensure = "Present"
+			Name = "Web-Http-Logging"
+		}
+		
+		#Install Web Security
+		WindowsFeature WebSecurity
+		{
+			Ensure = "Present"
+			Name = "Web-Security"
+		}
+
+		#Install Web App Dev
+		WindowsFeature WebAppDev
+		{
+			Ensure = "Present"
+			Name = "Web-App-Dev"
+		}
+
+
+		#Install Web HTTP Tracing
+		WindowsFeature WebHttpTracing
+
+		{
+			Ensure = "Present"
+			Name = "Web-Http-Tracing"
+		}
+
+		#Install Web Health
+		WindowsFeature WebHealth
+		{
+			Ensure = "Present"
+			Name = "Web-Health"
+		}
+
+
+		#Install Common HTTP Features 
+		WindowsFeature WebCommonHttp
+		{
+			Ensure = "Present"
+			Name = "Web-Common-Http"
+		}
+
+		#Install ASP.NET 4.5
+		WindowsFeature ASP45
+		{
+			Ensure = "Present"
+			Name = "Web-Asp-Net45"
+		}
+
+		#Install NET WCF TCP Port Sharing45
+		WindowsFeature NETWCFTCPPortSharing45
+		{
+			Ensure = "Present"
+			Name = "NET-WCF-TCP-PortSharing45"
+		}
+
+
+
+		#Install .NET Framework Core
+		WindowsFeature NETFrameworkCore
+		{
+			Ensure = "Present"
+			Name = "NET-Framework-Core"
+		}
+
+		#Install .NET Framework Features
+		WindowsFeature NETFramework45Features
+		{
+			Ensure = "Present"
+			Name = "NET-Framework-45-Features"
+		}
+
+		#Install .NET Framework 45 ASPNET
+		WindowsFeature NETFramework45ASPNET
+		{
+			Ensure = "Present"
+			Name = "NET-Framework-45-ASPNET"
+		}
+
+		#Install .NET 45 WCF Services
+		WindowsFeature NET-WCF-Services45
+		{
+			Ensure = "Present"
+			Name = "NET-WCF-Services45"
+		}
+
+		#Install ASP.NET 3.5
+		WindowsFeature ASP35
+		{
+			Ensure = "Present"
+			Name = "Web-Asp-Net"
+		}
+
+		#Install NET Extensibility 35
+		WindowsFeature NetExt35
+		{
+			Ensure = "Present"
+			Name = "Web-Net-Ext"
+		}
+		
+		#Install NET Extensibility 45
+		WindowsFeature NetExt45
+		{
+			Ensure = "Present"
+			Name = "Web-Net-Ext45"
+		}
+
+		#Install ISAPI Filters
+		WindowsFeature ISAPI_Filters
+		{
+			Ensure = "Present"
+			Name = "Web-ISAPI-Filter"
+		}
+
+		#Install ISAPI Extensions
+		WindowsFeature WebISAPI_EXT
+		{
+			Ensure = "Present"
+			Name = "Web-ISAPI-Ext"
+		}
+
+		#Install Default Document
+		WindowsFeature DefaultDocument
+		{
+			Ensure = "Present"
+			Name = "Web-Default-Doc"
+		}
+
+		#Install Static Content
+		WindowsFeature StaticContent
+		{
+			Ensure = "Present"
+			Name = "Web-Static-Content"
+		}
+
+		#Install AS HTTP Activation
+		WindowsFeature ASHTTPActivation
+		{
+			Ensure = "Present"
+			Name = "AS-HTTP-Activation"
+		}
+		
+		#Install Static Content Compression
+		WindowsFeature StaticContentCompression
+		{
+			Ensure = "Present"
+			Name = "Web-Stat-Compression"
+		}
+
+		#Install Request Filtering
+		WindowsFeature RequestFiltering
+		{
+			Ensure = "Present"		
+			Name = "Web-Filtering"
+		}
+
+		WindowsFeature WebServerManagementConsole
+		{
+			Ensure = "Present"	
+			Name = "Web-Mgmt-Console"
+		}
+
+#Azure Web Server config
+
+#New way
+#Install-AzurePowershellModules
+		Script InstallAzurePowershellModules
+        	{
+	            SetScript = 
+                    {  
+                        $trustrepo=Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+                        $install=install-module azure
+                        import-module azure 
+                    }
+	            GetScript =  { @{} }
+	            TestScript = 
+                    { 
+                          $module=get-module -listavailable -name azure -refresh -erroraction silentlycontinue
+                          if($module)
+                          {
+                            $true
+                          }
+                          else
+                          {
+                            $false
+                          }                        
+                    }
+                
+        	}
+    
+        xAzureBlobFiles DownloadDBAndVardata 
+        {
+            Path                    = "C:\downloads"
+            StorageAccountName      = $StorageAccountName
+            StorageAccountContainer = $StorageAccountContainer
+            StorageAccountKey       = $StorageAccountKey
+            DependsOn = "[Script]InstallAzurePowershellModules"
+        }
+        
+        xPendingReboot RebootAsNeeded
+        { 
+            Name = "Check for a pending reboot before changing anything" 
+        }
+
+		
+#		WindowsFeature WASNETEnvironment
+#		{
+#			Ensure = "Present"	
+#			Name = "WAS-NET-Environment"
+#		}
+	
+    }
+} 
