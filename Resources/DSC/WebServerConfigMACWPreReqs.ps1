a26b53dcf881b5f33725adc3e8a80ca057834a85
lpalerm <lisa.palermo@mckesson.com>
2017-08-09 07:14:36 -0500

    Remove TimeZone

--- Resources/DSC/WebServerConfigMACWPreReqs.ps1
+++ Resources/DSC/WebServerConfigMACWPreReqs.ps1
@@ -27,20 +27,7 @@
             Source = "$SourcePath"            
        }
      }
+    }
    }
-	  #Set-Eastern Standard Time
-
-        #SET-ITEMPROPERTY -Path 'HKLM:\System\CurrentControlSet\Control\TimeZoneInformation -name "TimeZoneKeyName" -	Value Eastern Standard Time -ErrorAction Stop
-        
-	Registry TimeZoneKeyName
-        {
-            Ensure = "Present"
-            Key = "HKLM:\System\CurrentControlSet\Control\TimeZoneInformation"
-            ValueName = "TimeZoneKeyName"
-            ValueData = "Eastern Standard Time"
-			ValueType = "String"
-        }
-
-#End Set-Eastern Time Zone
- }
+	 
 	 
