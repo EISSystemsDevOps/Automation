3b1f46e02b834d1f0c792a8eb54c69fef0d0f94b
lpalerm <lisa.palermo@mckesson.com>
2017-11-29 15:27:12 -0600

    Listen Tentacle

---
+++ Resources/DSC/ListeningTentacle.ps1
@@ -0,0 +1,34 @@
+﻿Configuration ListeningTentacle
+{
+    param ($OctopusServerUrl, $ApiKey, $Environments, $Roles, $ServerThumbprint)
+
+    Import-DscResource -ModuleName OctopusDSC
+
+    Node "localhost"
+    {
+      cTentacleAgent ListeningTentacle
+        {
+            Ensure = "Present";
+            State = "Started";
+
+            # Tentacle instance name. Leave it as 'Tentacle' unless you have more
+            # than one instance
+            Name = "Tentacle";
+
+            # Registration - all parameters required
+            ApiKey = $ApiKey;
+            OctopusServerUrl = $OctopusServerUrl;
+            Environments = $Environments;
+            Roles = $Roles;
+
+            # Optional settings
+            ListenPort = 10933;
+            DefaultApplicationDirectory = "C:\Applications"
+            PublicHostNameConfiguration = "FQDN"
+            TentacleHomeDirectory = "C:\Octopus"
+            RegisterWithServer = $true
+            OctopusServerThumbprint = $ServerThumbprint
+
+        }
+    }
+}
